3767426b50b333e455014ccc094575ea3f376d2e
Evgeniy Pashko <evgpashko92@gmail.com>
2022-05-22 01:20:17 +0700

    Custom App & Document files

---
+++ src/pages/_app.jsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const App = function ({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default App;
