9bfdb1dd6b38858300c68ed8488dc133db467dfe
Evgeniy Pashko <evgpashko92@gmail.com>
2022-05-26 23:58:31 +0700

    Добавлены белый и черный цвета

--- src/styles/default.js
+++ src/styles/default.js
@@ -1,4 +1,8 @@
 export default {
+  colors: {
+    white: "#ffffff",
+    black: "#000000",
+  },
   fontSize: {
     xs: ["0.75rem", { lineHeight: "1rem" }],
     sm: ["0.875rem", { lineHeight: "1.25rem" }],
